d736229780eacc5220d63cfd2ae28902fe3f3e23
Damup1988 <damup1988@gmail.com>
2021-06-29 22:23:35 +0300

    1st simple sollution

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i < 101; i++)
+            {                
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    System.Console.WriteLine("FizzBuzz");
+                    continue;
+                }            
+                if (i % 3 == 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                    continue;
+                }
+                if (i % 5 == 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                    continue;
+                }
+                System.Console.WriteLine(i);
+            }
         }
     }
 }
